6a5fd837c1b43dda572a165946724901e501b71e
Fedor Trubitsyn <energyv37@yandex.ru>
2019-04-28 14:09:52 +0200

    Details screen updated

--- app/src/main/java/club/bambilla/zimadtest/tabs/details/DetailsContract.java
+++ app/src/main/java/club/bambilla/zimadtest/tabs/details/DetailsContract.java
@@ -1,10 +1,12 @@
 package club.bambilla.zimadtest.tabs.details;
 
 import club.bambilla.zimadtest.base.BaseContract;
+import club.bambilla.zimadtest.models.ListItem;
 
 public interface DetailsContract {
     interface View extends BaseContract.View{
 
+        void showContent(ListItem listItem, int position);
     }
 
     interface Presenter extends BaseContract.Presenter<View>{
--- app/src/main/java/club/bambilla/zimadtest/tabs/details/DetailsFragment.java
+++ app/src/main/java/club/bambilla/zimadtest/tabs/details/DetailsFragment.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import club.bambilla.zimadtest.R;
 import club.bambilla.zimadtest.base.BaseMvpFragmentAbs;
@@ -18,6 +22,9 @@ public class DetailsFragment
     private static final String KEY_LIST_ITEM = TAG + "_LIST_ITEM";
     private static final String KEY_POSITION = TAG + "_POSITION";
 
+    private ImageView iconImageView;
+    private TextView positionTextView, titleTextView;
+
     public static DetailsFragment newInstance(ListItem listItem, int position) {
 
         Bundle args = new Bundle();
@@ -27,14 +34,46 @@ public class DetailsFragment
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     protected DetailsContract.Presenter getPresenter() {
-        return new DetailsPresenter();
+        if (getArguments() == null) {
+            throw new IllegalArgumentException("The arguments is null. Create fragment from 'newInstance'");
+        }
+
+        ListItem listItem = (ListItem) getArguments().getSerializable(KEY_LIST_ITEM);
+        int position = getArguments().getInt(KEY_POSITION);
+
+        return new DetailsPresenter(listItem, position);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_details_item, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initIds(view);
+    }
+
+    private void initIds(@NonNull View view) {
+        iconImageView = view.findViewById(R.id.image_view_icon);
+        positionTextView = view.findViewById(R.id.text_view_position);
+        titleTextView = view.findViewById(R.id.text_view_title);
+    }
+
+    @Override
+    public void showContent(ListItem listItem, int position) {
+        Picasso.get()
+                .load(listItem.getUrl())
+                .placeholder(android.R.drawable.stat_notify_sync)
+                .error(android.R.drawable.stat_notify_error)
+                .into(iconImageView);
+
+        positionTextView.setText(String.valueOf(position));
+        titleTextView.setText(listItem.getTitle());
     }
 }
