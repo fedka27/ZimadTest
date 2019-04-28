3db64cbc8878b804406817e1b16f268b3917da7c
Fedor Trubitsyn <energyv37@yandex.ru>
2019-04-28 12:43:17 +0200

    Attaching fragment updated to 'commit'

--- app/src/main/java/club/bambilla/zimadtest/MainActivity.java
+++ app/src/main/java/club/bambilla/zimadtest/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction
                 .disallowAddToBackStack()
-                .commitNowAllowingStateLoss();
+                .commit();
     }
 }
